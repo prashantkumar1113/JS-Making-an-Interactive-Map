7c49ecfb9da852b452a07c3b1255aa0f8906f2d9
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-02-09 18:45:16 -0600

    Added getting the user's geoloction

--- app.js
+++ app.js
@@ -4,3 +4,16 @@
 console.log("hello");
 
 //get user location
+async function getUserCoords() {
+    let pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+    //console.log([pos.coords.latitude, pos.coords.longitude]);
+    return [pos.coords.latitude, pos.coords.longitude];
+}
+
+// On load:
+window.onload = async () => {
+    const coords = await getUserCoords();
+    console.log(coords);
+};
