2697d5ec8b1b9399f016061b1093c02f4fb89965
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-02-09 18:36:25 -0600

    Added starter files, initial select list, and Bootstrap

---
+++ app.js
@@ -0,0 +1,6 @@
+// API's needed: Leaflet, Foursquare
+// Need to output a dropdownlist and submit button
+// Then draw the map with a user's geolocation
+console.log("hello");
+
+//get user location
